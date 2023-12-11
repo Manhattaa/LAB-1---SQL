44d082df92d9eee0165d820ca895f0a590929900
Fady <fadynhatta@gmail.com>
2023-12-11 11:42:24 +0100

    forgot to call on the EnterMethod in the switch case.
    Fixed that now.

--- LAB 1 - SQL/Program.cs
+++ LAB 1 - SQL/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LAB_1___SQL.Methods.GradesAndClasses;
 using LAB_1___SQL.Methods.Staff;
 using LAB_1___SQL.Methods.Students;
+using LAB_1___SQL.Utilities;
 
 namespace LAB_1___SQL
 {
@@ -32,30 +33,38 @@ namespace LAB_1___SQL
             {
                 case 1:
                     ListAllStudents.ListStudents();
+                    EnterMethod.Enter();
                     break;
                 case 2:
                     StudentClass.ListStudentsInClasses();
+                    EnterMethod.Enter();
                     break;
                 case 3:
                     HireNewStaff.HireStaff();
+                    EnterMethod.Enter();
                     break;
                 case 4:
                     ListAllStaff.ListStaff();
+                    EnterMethod.Enter();
                     break;
                 case 5:
                     ListGrades.ThirtyDayGrades();
+                    EnterMethod.Enter();
                     break;
                 case 6:
                     AvgGrade.ListAvgGrades();
+                    EnterMethod.Enter();
                     break;
                 case 7:
                     AddNewStudents.EnrollStudent();
+                    EnterMethod.Enter();
                     break;
                 case 0:
                     Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("Invalid choice. Please try again.");
+                    EnterMethod.Enter();
                     break;
 
             }
